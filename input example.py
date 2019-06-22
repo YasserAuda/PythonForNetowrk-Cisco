5a958e9990c019bfdff4e84e84d750a9b5d2e2e9
Yasser Auda Lab <yasserauda73@hotmail.com>
2019-06-22 18:56:52 +0400

    Add files via upload

---
+++ input example.py
@@ -0,0 +1,7 @@
+#CCIE/CCSI:Yasser Ramzy Auda
+#https://www.facebook.com/yasser.auda
+#https://www.linkedin.com/in/yasserauda/
+#https://github.com/YasserAuda/PythonForNetowrk-Cisco
+
+firstname=input("what is your name?")
+print("Hi "+firstname+"!")
