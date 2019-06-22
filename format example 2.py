bde9aa6ae0d24c142bf1dcde1e0d11799ea18ae4
Yasser Auda Lab <yasserauda73@hotmail.com>
2019-06-22 18:56:28 +0400

    Add files via upload

---
+++ format example 2.py
@@ -0,0 +1,9 @@
+#CCIE/CCSI:Yasser Ramzy Auda
+#https://www.facebook.com/yasser.auda
+#https://www.linkedin.com/in/yasserauda/
+#https://github.com/YasserAuda/PythonForNetowrk-Cisco
+
+x = format(255, 'x')
+print(x)
+
+#Format 255 into a hexadecimal value:
