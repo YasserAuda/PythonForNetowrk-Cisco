b52ba705ab8519156d124f9762bc2ada77fd966d
Yasser Auda Lab <yasserauda73@hotmail.com>
2019-06-22 18:55:51 +0400

    Add files via upload

---
+++ format example 1.py
@@ -0,0 +1,10 @@
+#CCIE/CCSI:Yasser Ramzy Auda
+#https://www.facebook.com/yasser.auda
+#https://www.linkedin.com/in/yasserauda/
+#https://github.com/YasserAuda/PythonForNetowrk-Cisco
+
+x = format(0.5, '%')
+print(x)
+
+#format(value, format)
+
